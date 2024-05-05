d4aec34a1c31fff17364f79e2ef564cdded6ec80
Yuvraj Singh Galley <156587604+Ghostcoder59@users.noreply.github.com>
2024-05-05 13:55:36 +0530

    Create HANGMAN.py

---
+++ HANGMAN.py
@@ -0,0 +1,145 @@
+print("TEST FOR BTS FANS")
+import random
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+end_of_game = False
+word_list = ["dynamite",
+            "boy with luv",
+            "fake love",
+            "blood sweat & tears",
+            "idol",
+            "mic drop",
+            "dna",
+            "spring day",
+            "fire",
+            "not today",
+            "save me",
+            "on",
+            "black swan",
+            "butter",
+            "permission to dance",
+            "life goes on",
+            "euphoria",
+            "make it right",
+            "run",
+            "i need u",
+            "stay gold",
+            "film out",
+            "we are bulletproof: the eternal",
+            "go go",
+            "epiphany",
+            "serendipity",
+            "best of me",
+            "anpanman",
+            "magic shop",
+            "love maze",
+            "your eyes tell",
+            "lights",
+            "rm",
+            "jin",
+            "suga",
+            "j-hope",
+            "jimin",
+            "v",
+            "jungkook"]
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+#TODO-1: - Create a variable called 'lives' to keep track of the number of lives left. 
+#Set 'lives' to equal 6.
+lives = 6
+
+#Testing code
+# print(f'Pssst, the solution is {chosen_word}.')
+
+#Create blanks
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    #Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+       # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
+
+    #TODO-2: - If guess is not a letter in the chosen_word,
+    #Then reduce 'lives' by 1. 
+    #If lives goes down to 0 then the game should stop and it should print "You lose."
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+
+    #Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    #Check if user has got all letters.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    #TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
+    print(stages[lives])
+print(f'Pssst, the solution is {chosen_word}.')
